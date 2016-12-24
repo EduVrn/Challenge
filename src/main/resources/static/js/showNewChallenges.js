00367628268b584f342645528c02c7db7f23a6b2
natalya-erina <natalya.erina.96@gmail.com>
2016-12-25 02:14:32 +0300

    Added notifications

--- src/main/resources/static/js/showNewChallenges.js
+++ src/main/resources/static/js/showNewChallenges.js
@@ -1,3 +1,11 @@
+var showing = false;
+
 $('#notification').click(function() {
+    showing = !showing;
+    if (showing) {
+        $('#chal-notification').show();
+    } else {
+        $('#chal-notification').hide();
+    }
 });
 
