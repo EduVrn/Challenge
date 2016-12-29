432cf2686abb4ad3311ed06fdf14989a31a8b7c3
DNS <DNS@Lenovo-PC>
2016-12-29 20:50:27 +0300

    multilang option added (rus/en)

--- src/main/java/challenge/Application.java
+++ src/main/java/challenge/Application.java
@@ -16,104 +16,21 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import challenge.dbside.services.ini.MediaService;
+import java.util.Arrays;
 
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
 public class Application extends SpringBootServletInitializer {
 
-    private static InitialLoader someThing;
+    private static InitialLoader initiator;
 
     @Autowired
     public void setSomeThing(InitialLoader someThing) {
-        Application.someThing = someThing;
+        Application.initiator = someThing;
     }
 
-    static void Init(ApplicationContext context) {
-        MediaService serviceEntity = (MediaService) context.getBean("storageServiceUser");
-        /*Example entity*/
-        User user1 = new User();
-        serviceEntity.save(user1);
-
-        User user2 = new User();
-        user2.setName("name2");
-        serviceEntity.save(user2);
-
-        User user3 = new User();
-        user3.setName("name3");
-        serviceEntity.save(user3);
-
-        User user4 = new User();
-        user4.setName("name4");
-        serviceEntity.save(user4);
-
-        System.out.println("\n\nUpdate");
-        user1.setName("new_name");
-        //user1.setParent(user2);
-        serviceEntity.update(user1);
-
-        user4.setName("new_name4");
-        //user4.setParent(user2);
-        serviceEntity.update(user4);
-
-        Set userSet = new HashSet();
-        userSet.add(user1);
-        userSet.add(user4);
-        user2.setChildren(userSet);
-        serviceEntity.update(user2);
-
-        /**/
-        System.out.println("\n\nDelete");
-        serviceEntity.delete(user1);
-
-        System.out.println("\n\nGet all");
-        /**
-         * *TEMPLAAAAAAAAAAAAAAAAAAAAAAATE !!!!!!***
-         */
-        List<User> list = serviceEntity.getAll(User.class);
-        for (User b : list) {
-            System.out.println(b);
-        }
-
-        System.out.println("\n\nDelete");
-        serviceEntity.delete(user2);
-
-        System.out.println("\n\nGet all");
-        /**
-         * *TEMPLAAAAAAAAAAAAAAAAAAAAAAATE !!!!!!***
-         */
-        list = serviceEntity.getAll(User.class);
-        for (BaseEntity b : list) {
-            System.out.println(b);
-        }
-
-        User user6 = new User();
-        user6.setName("name6");
-        serviceEntity.save(user6);
-        ChallengeDefinition m = new ChallengeDefinition();
-        serviceEntity.save(m);
-        user3.addChallenge(m);
-        user3.addFriend(user6);
-        serviceEntity.update(user3);
-
-        System.out.println("modified users: \n");
-        System.out.println(user3.toString());
-        System.out.println(user6.toString());
-
-        ChallengeDefinition challengeRoadMap1 = new ChallengeDefinition();
-        challengeRoadMap1.setName("challengeName1");
-        serviceEntity.save(challengeRoadMap1);
-
-        /**
-         * *TEMPLAAAAAAAAAAAAAAAAAAAAAAATE !!!!!!***
-         */
-        list = serviceEntity.getAll(User.class);
-
-        for (User b : list) {
-            System.out.println(b);
-        }
-    }
-
+    @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(Application.class);
     }
@@ -122,8 +39,17 @@ public class Application extends SpringBootServletInitializer {
         //ApplicationContext context = new ClassPathXmlApplicationContext("META-INF/beans.xml");
 
         //Init();
-        SpringApplication.run(Application.class, args);
-        someThing.initial();
+        // SpringApplication.run(Application.class, args);
+        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+
+        System.out.println("All created beans:");
+
+        String[] beanNames = ctx.getBeanDefinitionNames();
+        Arrays.sort(beanNames);
+        for (String beanName : beanNames) {
+            System.out.println(beanName);
+        }
+        initiator.initial();
     }
 
 }
--- src/main/java/challenge/webside/controllers/MainController.java
+++ src/main/java/challenge/webside/controllers/MainController.java
@@ -1,15 +1,12 @@
 package challenge.webside.controllers;
 
 import challenge.dbside.models.ChallengeDefinition;
-import challenge.dbside.models.User;
 import challenge.dbside.services.ini.MediaService;
 import org.springframework.social.connect.Connection;
 import org.springframework.social.connect.ConnectionRepository;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.context.request.WebRequest;
 import challenge.webside.controllers.util.SocialControllerUtil;
-import challenge.webside.model.UserConnection;
-import challenge.webside.model.UserProfile;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -21,9 +18,10 @@ import java.security.Principal;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
-import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.web.bind.WebDataBinder;
@@ -31,6 +29,8 @@ import org.springframework.web.bind.annotation.InitBinder;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Controller
 public class MainController {
@@ -50,7 +50,7 @@ public class MainController {
         util.setModelForMain(request, currentUser, model);
         return "main";
     }
-    
+
     @RequestMapping(value = "challenge/information", method = GET, produces = "text/plain;charset=UTF-8")
     public String show(HttpServletRequest request, Principal currentUser, Model model, @RequestParam("id") int id) {
         util.setModelForChallengeShow(id, request, currentUser, model);
@@ -67,6 +67,18 @@ public class MainController {
     public String newChal(HttpServletRequest request, Principal currentUser, Model model) {
         util.setModelForNewChallenge(request, currentUser, model);
         return "chalNewOrUpdate";
+    }
+
+    protected Optional<String> getPreviousPageByRequest(HttpServletRequest request) {
+        return Optional.ofNullable(request.getHeader("Referer")).map(requestUrl -> "redirect:" + requestUrl);
+    }
+
+    @RequestMapping(value = "changelang", produces = "text/plain;charset=UTF-8", method = GET)
+    public String changeLang(HttpServletRequest request, Principal currentUser, Model model,@RequestParam("lang") String lang) {
+      SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.forLanguageTag(lang));
+      
+        return getPreviousPageByRequest(request).orElse("/");
     }
 
     @RequestMapping(value = "challenge/information", method = POST, produces = "text/plain;charset=UTF-8")
@@ -86,71 +98,67 @@ public class MainController {
         util.setModel(request, currentUser, model);
         return "login";
     }
-    
-    @RequestMapping(value="/accept", method=GET, produces = "text/plain;charset=UTF-8")
+
+    @RequestMapping(value = "/accept", method = GET, produces = "text/plain;charset=UTF-8")
     public String accept(HttpServletRequest request, Principal currentUser, Model model, @RequestParam("id") int chalId) {
         util.setModelForAcceptOrDeclineChallenge(request, currentUser, model, chalId, true);
         return "profile";
     }
-    
-    @RequestMapping(value="/decline", method=GET, produces = "text/plain;charset=UTF-8")
+
+    @RequestMapping(value = "/decline", method = GET, produces = "text/plain;charset=UTF-8")
     public String decline(HttpServletRequest request, Principal currentUser, Model model, @RequestParam("id") int chalId) {
         util.setModelForAcceptOrDeclineChallenge(request, currentUser, model, chalId, false);
         return "profile";
     }
-    
-    @RequestMapping(value="/acceptDefinition", method=GET, produces = "text/plain;charset=UTF-8")
+
+    @RequestMapping(value = "/acceptDefinition", method = GET, produces = "text/plain;charset=UTF-8")
     public String acceptChallengeDefinition(HttpServletRequest request, Principal currentUser, Model model, @RequestParam("id") int chalId) {
         util.setModelForAcceptChallengeDefinition(request, currentUser, model, chalId);
         return "profile";
     }
-   
-    
-    @RequestMapping(value="/user4Challenge", method=GET, produces = "text/plain;charset=UTF-8")
+
+    @RequestMapping(value = "/user4Challenge", method = GET, produces = "text/plain;charset=UTF-8")
     public String selectUserCandidate4Challenge(HttpServletRequest request, Principal currentUser, Model model, @RequestParam("id") int chalId) {
-    	util.setModelForThrowChallenge2User(request, currentUser, model, chalId);
-    	
-    	return "listSomething";
+        util.setModelForThrowChallenge2User(request, currentUser, model, chalId);
+
+        return "listSomething";
     }
-    
-    
-    @RequestMapping(value="/challenge4User", method=GET, produces = "text/plain;charset=UTF-8")
+
+    @RequestMapping(value = "/challenge4User", method = GET, produces = "text/plain;charset=UTF-8")
     public String selectChallengeCandidate4User(HttpServletRequest request, Principal currentUser, Model model, @RequestParam("id") int userId) {
-    	util.setModelForThrowUser2Challenge(request, currentUser, model, userId);    	
-    	return "listSomething";
+        util.setModelForThrowUser2Challenge(request, currentUser, model, userId);
+        return "listSomething";
     }
-    
-    @RequestMapping(value="/throwChallengeFromChallengeList", method=GET, produces = "text/plain;charset=UTF-8")
-    public String throwChallenge2UserFromChallengeList(HttpServletRequest request, Principal currentUser, Model model, 
-    		@RequestParam("idParent") int challengeId, @RequestParam("id") int userId) {
-    	
-    	//TODO: need make any class with handler creation relation (friends, challenge2User, ...)
-    	util.throwChallenge2User(userId, challengeId);
-    	//TODO: get user id current seanse, this current id - is id user, which throw challenge
-    	util.setProfileShow(userId, request, currentUser, model);
-    	return "profile";
+
+    @RequestMapping(value = "/throwChallengeFromChallengeList", method = GET, produces = "text/plain;charset=UTF-8")
+    public String throwChallenge2UserFromChallengeList(HttpServletRequest request, Principal currentUser, Model model,
+            @RequestParam("idParent") int challengeId, @RequestParam("id") int userId) {
+
+        //TODO: need make any class with handler creation relation (friends, challenge2User, ...)
+        util.throwChallenge2User(userId, challengeId);
+        //TODO: get user id current seanse, this current id - is id user, which throw challenge
+        util.setProfileShow(userId, request, currentUser, model);
+        return "profile";
     }
-    
-    @RequestMapping(value="/throwChallengeFromUserList", method=GET, produces = "text/plain;charset=UTF-8")
-    public String throwChallenge2UserFromUserList(HttpServletRequest request, Principal currentUser, Model model, 
-    		@RequestParam("idParent") int userId, @RequestParam("id") int challengeId) {
-    	
-    	//TODO: need make any class with handler creation relation (friends, challenge2User, ...)
-    	util.throwChallenge2User(userId, challengeId);
-    	//TODO: get user id current seanse, this current id - is id user, which throw challenge
-    	util.setProfileShow(userId, request, currentUser, model);
-    	return "profile";
+
+    @RequestMapping(value = "/throwChallengeFromUserList", method = GET, produces = "text/plain;charset=UTF-8")
+    public String throwChallenge2UserFromUserList(HttpServletRequest request, Principal currentUser, Model model,
+            @RequestParam("idParent") int userId, @RequestParam("id") int challengeId) {
+
+        //TODO: need make any class with handler creation relation (friends, challenge2User, ...)
+        util.throwChallenge2User(userId, challengeId);
+        //TODO: get user id current seanse, this current id - is id user, which throw challenge
+        util.setProfileShow(userId, request, currentUser, model);
+        return "profile";
     }
-    
-    
-    @RequestMapping(value="/friends", method=GET, produces = "text/plain;charset=UTF-8")
-    public String selectUserFriends(HttpServletRequest request, Principal currentUser, Model model, @RequestParam("id") int userId) {    	
-    	util.setModelForShowFriends(request, currentUser, model, userId);
-    	
-    	return "listSomething";
+
+    @RequestMapping(value = "/friends", method = GET, produces = "text/plain;charset=UTF-8")
+    public String selectUserFriends(HttpServletRequest request, Principal currentUser, Model model, @RequestParam("id") int userId) {
+        util.setModelForShowFriends(request, currentUser, model, userId);
+
+        return "listSomething";
     }
-    
-    
+
     @InitBinder
     public void initBinder(WebDataBinder binder) {
         CustomDateEditor editor = new CustomDateEditor(new SimpleDateFormat("yyyy/MM/dd HH:mm"), true);
@@ -174,8 +182,5 @@ public class MainController {
 
         return "index";
     }
-    
-    
-    
-    
+
 }
