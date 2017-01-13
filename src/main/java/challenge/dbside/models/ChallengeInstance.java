43fa5246905bdc84878b12a9b298a730307da91b
DNS <DNS@Lenovo-PC>
2017-01-13 14:07:50 +0300

    global changes in layout structure (fragmentation, refactoring)

--- src/main/java/challenge/dbside/models/ChallengeInstance.java
+++ src/main/java/challenge/dbside/models/ChallengeInstance.java
@@ -1,40 +1,40 @@
 package challenge.dbside.models;
 
-
 import challenge.dbside.models.common.IdAttrGet;
 import challenge.dbside.models.dbentity.DBSource;
 import challenge.dbside.models.status.ChallengeStatus;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import java.util.List;
-
+import java.util.Locale;
 
 public class ChallengeInstance extends BaseEntity {
 
-		
     public ChallengeInstance() {
         super(ChallengeInstance.class.getSimpleName());
     }
-    
+
     public ChallengeInstance(DBSource dataSource) {
-    	super(dataSource);
+        super(dataSource);
     }
-    
+
     public ChallengeInstance(ChallengeDefinition chalDef) {
         super(ChallengeInstance.class.getSimpleName());
         setName(chalDef.getName());
     }
-    
+
     public ChallengeDefinition getChallengeRoot() {
-    	return new ChallengeDefinition(getDataSource().getParent());
+        return new ChallengeDefinition(getDataSource().getParent());
     }
-    
+
     public void setChallengeRoot(ChallengeDefinition rootChallenge) {
-    	getDataSource().setParent(rootChallenge.getDataSource());
+        getDataSource().setParent(rootChallenge.getDataSource());
     }
-    
-    
-    public String getName() {     
-        return (String)getDataSource().getAttributes().get(IdAttrGet.IdName()).getValue();
+
+    public String getName() {
+        return (String) getDataSource().getAttributes().get(IdAttrGet.IdName()).getValue();
     }
 
     public void setName(String name) {
@@ -42,21 +42,55 @@ public class ChallengeInstance extends BaseEntity {
     }
 
     public User getAcceptor() {
-    	List list = (List<DBSource>)getDataSource().getBackRel().get(IdAttrGet.refAcChalIns());
-    	
-    	DBSource userDB = (DBSource) (list.get(0));
-    	return new User(userDB);
+        List list = (List<DBSource>) getDataSource().getBackRel().get(IdAttrGet.refAcChalIns());
+
+        DBSource userDB = (DBSource) (list.get(0));
+        return new User(userDB);
     }
 
-    public void setAcceptor(User acceptor) {    	
-    	getDataSource().getBackRel().put(IdAttrGet.refAcChalIns(), acceptor.getDataSource());
+    public void setAcceptor(User acceptor) {
+        getDataSource().getBackRel().put(IdAttrGet.refAcChalIns(), acceptor.getDataSource());
     }
-    
+
     public ChallengeStatus getStatus() {
         return ChallengeStatus.valueOf(getDataSource().getAttributes().get(IdAttrGet.IdChalStat()).getValue());
     }
-    
+
     public void setStatus(ChallengeStatus status) {
         getDataSource().getAttributes().get(IdAttrGet.IdChalStat()).setValue(status.name());
+    }
+
+    public String getDescription() {
+        return getDataSource().getAttributes().get(IdAttrGet.IdDescr()).getValue();
+    }
+
+    public void setDescription(String description) {
+        getDataSource().getAttributes().get(IdAttrGet.IdDescr()).setValue(description);
+    }
+
+    public String getImageRef() {
+        return "../images/" + getDataSource().getAttributes().get(IdAttrGet.IdImgRef()).getValue();
+    }
+
+    public void setImageRef(String image) {
+        getDataSource().getAttributes().get(IdAttrGet.IdImgRef()).setValue(image);
+    }
+
+    public Date getDate() {
+        try {
+            DateFormat df = new SimpleDateFormat("EEE MMM dd kk:mm:ss z yyyy", Locale.ENGLISH);
+            String ddt = getDataSource().getAttributes().get(IdAttrGet.IdDate()).getValue();
+            Date result = df.parse(ddt);
+            return result;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return (new Date(0));
+            //new Date() == current date,
+            //return (new Date());
+        }
+    }
+
+    public void setDate(Date date) {
+        getDataSource().getAttributes().get(IdAttrGet.IdDate()).setValue(date.toString());
     }
 }
