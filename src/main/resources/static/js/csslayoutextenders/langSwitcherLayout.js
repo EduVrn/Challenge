ff84b1f63e8d889aa232e829568eb4dbf55c5408
dkondagit <fotoneg@yandex.ru>
2017-01-15 00:45:09 +0300

    minor layout changes

---
+++ src/main/resources/static/js/csslayoutextenders/langSwitcherLayout.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    $(".lang-button").addClass("btn btn-default col-md-6 col-sm-6 col-xs-6");
+   
+});
+
--- src/main/resources/static/js/csslayoutextenders/profileLayout.js
+++ src/main/resources/static/js/csslayoutextenders/profileLayout.js
@@ -1,6 +1,10 @@
 $(document).ready(function () {
     $(".avatar").addClass("col-md-4 col-sm-6 col-xs-12");
-    $(".rate-iconP").addClass("text-center col-md-3 col-sm-3 col-xs-3").removeClass("rate-iconP");
+    $(".avatar-image").addClass("text-center col-md-12 col-sm-12 col-xs-12");
+    $(".underAvatarButton").addClass("btn btn-default text-center col-md-6 col-sm-6 col-xs-6");
+    $(".rate-iconP").addClass("text-center col-md-6 col-sm-6 col-xs-6").removeClass("rate-iconP");
     $(".rate-blockP").addClass("col-md-12 col-sm-12 col-xs-12").removeClass("rate-blockP");
+    $(".elementOfListInProfile").addClass("challenge col-lg-4 col-md-4 col-sm-6 col-xs-12");
+    $(".userLists").addClass("col-md-8 col-sm-6 col-xs-12");
 });
 
