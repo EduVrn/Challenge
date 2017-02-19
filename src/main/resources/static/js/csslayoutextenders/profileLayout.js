263a83370c1c13df41c7ff47ea10b439a019c021
dkondagit <fotoneg@yandex.ru>
2017-02-19 20:53:07 +0300

    Global changes in layout(conception + elements). Mobile layout improved.

--- src/main/resources/static/js/csslayoutextenders/profileLayout.js
+++ src/main/resources/static/js/csslayoutextenders/profileLayout.js
@@ -2,10 +2,10 @@ $(document).ready(function () {
     $(".avatar").addClass("col-md-4 col-sm-6 col-xs-12");
     $(".avatar-image").addClass("text-center col-md-12 col-sm-12 col-xs-12");
     $(".bind-account").addClass("col-md-6 col-sm-6 col-xs-6");
-    $(".underAvatarButton").addClass("btn btn-default text-center col-md-6 col-sm-6 col-xs-6");
+    $(".underAvatarButton").addClass("btn btn-default text-center col-md-12 col-sm-12 col-xs-12");
     $(".rate-iconP").addClass("text-center col-md-6 col-sm-6 col-xs-6").removeClass("rate-iconP");
     $(".rate-blockP").addClass("col-md-12 col-sm-12 col-xs-12").removeClass("rate-blockP");
-    $(".elementOfListInProfile").addClass("challenge col-lg-4 col-md-4 col-sm-6 col-xs-12");
+    $(".elementOfListInProfile").addClass("challenge col-lg-6 col-md-6 col-sm-12 col-xs-12");
     $(".userLists").addClass("col-md-8 col-sm-6 col-xs-12");
 });
 
